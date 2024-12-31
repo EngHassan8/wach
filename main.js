bb0f184f04c0d748eb2bc0099d6d4c79e8257c2b
EngHassan8 <hassannaana99@gmail>
2024-12-31 04:30:51 -0800

    waa sacad shaqeyneyso

---
+++ main.js
@@ -0,0 +1,15 @@
+let  box = document.querySelector('#ligh')
+let  button1 = document.querySelector('#btnOn')
+let  button2= document.querySelector('#btnOff')
+
+
+
+///mesha wax lugu daroo
+
+button1.addEventListener("click",function(){
+   box.style.backgroundColor = "yellow"
+})
+
+butto2.addEventListener("click" ,function(){
+    box.style.backgroundColor= "transto"
+})
